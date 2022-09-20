3994a129fe849be524e4ac04c77d7568358fecdc
Maria Clara Rosa Meier <105031629+clarameier@users.noreply.github.com>
2022-09-20 18:50:13 -0300

    codigo extra sobre CORES

---
+++ class11.0.py
@@ -0,0 +1,8 @@
+print('\033[1;30;47mOlá, Mundo!\033[m')
+
+a = 3
+b = 5
+print('Os valores são \033[32m{}\033[m e \033[31m{}\033[m!'.format(a, b))
+
+x = 'curso de python no cursoemvideo'
+print(x[:5])
